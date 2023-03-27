b3f3728ca677a33d75b1052e59ae2fa800e3428d
jam <muhammad.nadeem@synavos.com>
2023-03-27 22:11:21 +0500

    Fix Social link validation issue

--- src/validators/entities/seller/register/social-links.js
+++ src/validators/entities/seller/register/social-links.js
@@ -1,32 +1,32 @@
 export const socialLinks = {
     facebook: {
         type: 'string',
-        format: "url",
+        // format: "url",
         errorMessage: "Please enter valid Facebook social link."
     },
     instagram: {
         type: 'string',
-        format: "url",
+        // format: "url",
         errorMessage: "Please enter valid Instagram social link."
     },
     youtube: {
         type: 'string',
-        format: "url",
+        // format: "url",
         errorMessage: "Please enter valid Youtube social link."
     },
     twitter: {
         type: 'string',
-        format: "url",
+        // format: "url",
         errorMessage: "Please enter valid Twitter social link."
     },
     linkedIn: {
         type: 'string',
-        format: "url",
+        // format: "url",
         errorMessage: "Please enter valid LinkedIn social link."
     },
     tikTok: {
         type: 'string',
-        format: "url",
+        // format: "url",
         errorMessage: "Please enter valid TikTok social link."
     },
 }
